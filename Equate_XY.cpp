2c311eeb7d0bfddc9d66104d2b516aff3d166bfa
yashjain0313 <yashjain0313@gmail.com>
2025-06-26 07:10:01 +0530

    D

---
+++ Equate_XY.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define endl '\n'
+#define int long long
+#define arrin(a, n)             \
+    for (int i = 0; i < n; i++) \
+        cin >> a[i];
+
+void solve()
+{
+ 
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
